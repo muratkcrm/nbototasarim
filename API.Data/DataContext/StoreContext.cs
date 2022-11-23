ed05e3b3c29feb2cdacac9d99fd2fc3da00efea5
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2022-11-23 17:49:48 +0300

    v-1

---
+++ API.Data/DataContext/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Data.DataContext
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
