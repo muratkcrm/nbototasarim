6cc88e675ca8d4269416a466ad3e068db5147841
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2022-11-23 18:08:27 +0300

    v1-3

--- API.Data/DbModels/Product.cs
+++ API.Data/DbModels/Product.cs
@@ -8,7 +8,7 @@ namespace API.Data.DbModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Aciklama { get; set; }
+        public string Description { get; set; }
         public string Category { get; set; }
     }
 }
