398ecffeea0bf7c55b7c9f9552da4b350c2bb8d1
muratkcrm <muratkcrm@gmail.com>
2022-11-25 17:16:48 +0300

    V-1-25112022-1716

---
+++ API.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllProductsAsync();
+    }
+}
---
+++ API.Infrastructure/Implements/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Implements
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IReadOnlyList<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
