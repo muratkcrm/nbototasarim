a4ea234d85f897f9d5191824343d99b6991af597
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2022-11-26 19:41:32 +0300

    v-26112022-1941

--- API.Core/DbModels/Product.cs
+++ API.Core/DbModels/Product.cs
@@ -1,15 +1,20 @@
-﻿namespace API.Core.DbModels
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Core.DbModels
 {
     public class Product:BaseEntity
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public decimal Price { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Price { get; set; }
         public string PictureUrl { get; set; }
         public ProductType ProductType { get; set; }
-        public int ProductTypeId { get; set; }
-        public ProductBrand ProductBrand { get; set; }
+        public int? ProductTypeId { get; set; }
+        public ProductBrand GetProductBrand { get; set; }
         public int ProductBrandId { get; set;}
     }
 }
