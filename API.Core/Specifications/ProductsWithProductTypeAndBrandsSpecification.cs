9dd77576c30cb8ef1f668a52ea0c30a03daa33c0
muratkcrm <muratkcrm@gmail.com>
2022-12-06 17:50:45 +0300

    v-06122022-1750

--- API.Core/Specifications/ProductsWithProductTypeAndBrandsSpecification.cs
+++ API.Core/Specifications/ProductsWithProductTypeAndBrandsSpecification.cs
@@ -12,8 +12,23 @@ namespace API.Core.Specifications
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
-            AddOrderBy(x => x.Name);
-            
+            //AddOrderBy(x => x.Name);
+            if (!string.IsNullOrWhiteSpace(sort)) 
+            {
+                switch(sort) 
+                {
+                    case "priceAsc":
+                        AddOrderBy(x => x.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDesending(x => x.Price);
+                        break;
+                    default: 
+                        AddOrderBy(x => x.Name); 
+                        break;
+
+                }
+            }
         }
         public ProductsWithProductTypeAndBrandsSpecification(int id)
             :base(x=> x.Id == id)
