aedbd32ea8892efcea85b515e11a7a8a07f110da
muratkcrm <muratkcrm@gmail.com>
2022-12-06 18:10:25 +0300

    v-06122022-1810

--- API.Core/Specifications/ProductsWithProductTypeAndBrandsSpecification.cs
+++ API.Core/Specifications/ProductsWithProductTypeAndBrandsSpecification.cs
@@ -8,7 +8,10 @@ namespace API.Core.Specifications
 {
     public class ProductsWithProductTypeAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithProductTypeAndBrandsSpecification(string sort)
+        public ProductsWithProductTypeAndBrandsSpecification(string sort, int? brandId, int? typeId)
+            :base (x=> (!brandId.HasValue || x.ProductBrandId == brandId)
+            &&
+            (!typeId.HasValue || x.ProductTypeId == typeId))
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -33,9 +33,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithProductTypeAndBrandsSpecification(sort);
+            var spec = new ProductsWithProductTypeAndBrandsSpecification(sort, brandId, typeId);
             var data = await _productRepository.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(data));
 
