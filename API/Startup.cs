8922050923a86f0acdfe0d5efa0cf2a38e05f6f1
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2022-12-19 00:19:11 +0300

    v-19122022-0018-netcore

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using System.IO;
 
 namespace API
 {
@@ -23,7 +26,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllersWithViews();
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
