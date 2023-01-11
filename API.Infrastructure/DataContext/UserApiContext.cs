c5dffa4252f78b6dd6d262079ee3e4e45ce5d735
muratkcrm <muratkcrm@gmail.com>
2023-01-11 17:29:25 +0300

    11012023-1721-angular

---
+++ API.Infrastructure/DataContext/UserApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Infrastructure.DataContext
+{
+    public class UserApiContext:DbContext
+    {
+        public UserApiContext(DbContextOptions<UserApiContext> options) : base(options)
+        {
+
+        }
+    }
+}
