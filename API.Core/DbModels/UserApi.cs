94030189c388769b41c4af19d617dc74cd7f5828
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2023-01-12 00:50:33 +0300

    v-12012023-0050-login

---
+++ API.Core/DbModels/UserApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace API.Core.DbModels
+{
+    public class UserApi:BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
